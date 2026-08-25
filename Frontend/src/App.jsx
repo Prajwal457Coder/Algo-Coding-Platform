9570b6d4db8d5f3ce16814e9801eb97c87da1d8d
Prajwal Kumbhare <prajwalkumbhare457@gmail.com>
2026-08-25 13:51:57 +0530

    final changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,61 +1,3 @@
-// import {Routes, Route ,Navigate} from "react-router";
-// import Login from "./pages/Login";
-// import Signup from "./pages/Signup";
-// import Homepage from "./pages/Homepage";
-// import { useDispatch, useSelector } from 'react-redux';
-// import { checkAuth } from "./authSlice";
-// import { useEffect } from "react";
-// import AdminPanel from "./components/AdminPanel";
-// import ProblemPage from "./pages/ProblemPage"
-// import Admin from "./pages/Admin";
-// import AdminDelete from "./components/AdminDelete"
-// import AdminUpdate from "./components/AdminUpdate"
-// import AdminVideo from "./components/AdminVideo"
-// import UpdateProblem from "./components/UpdateProblem";
-// import AdminUpload from "./components/AdminUpload"
-// function App(){
-  
-//   const dispatch = useDispatch();
-//   const {isAuthenticated,user,loading} = useSelector((state)=>state.auth);
-
-//   // check initial authentication
-//   useEffect(() => {
-//     dispatch(checkAuth());
-//   }, [dispatch]);
-  
-//   if (loading) {
-//     return <div className="min-h-screen flex items-center justify-center">
-//       <span className="loading loading-spinner loading-lg"></span>
-//     </div>;
-//   }
-
-//   return(
-//   <>
-//     <Routes>
-//       <Route path="/" element={isAuthenticated ?<Homepage></Homepage>:<Navigate to="/signup" />}></Route>
-//       <Route path="/login" element={isAuthenticated?<Navigate to="/" />:<Login></Login>}></Route>
-//       <Route path="/signup" element={isAuthenticated?<Navigate to="/" />:<Signup></Signup>}></Route>
-//       <Route path="/admin" element={isAuthenticated && user?.role === 'admin' ? <Admin /> : <Navigate to="/" />} />
-//       <Route path="/admin/create" element={isAuthenticated && user?.role === 'admin' ? <AdminPanel /> : <Navigate to="/" />} />
-//       <Route path="/admin/delete" element={isAuthenticated && user?.role === 'admin' ? <AdminDelete /> : <Navigate to="/" />} />
-//       <Route path="/admin/update" element={isAuthenticated && user?.role === 'admin' ? <AdminUpdate /> : <Navigate to="/" />} />
-//        <Route path="/admin/upload/:problemId" element={isAuthenticated && user?.role === 'admin' ? <AdminUpload /> : <Navigate to="/" />} />
-//       <Route path="/admin/update/:id" element={isAuthenticated && user?.role === 'admin' ? <UpdateProblem /> : <Navigate to="/" />} />
-//       <Route path="/problem/:problemId" element={<ProblemPage/>}></Route>
-//       <Route path="/admin/video" element={isAuthenticated && user?.role === 'admin' ? <AdminVideo /> : <Navigate to="/" />} />
-      
-//     </Routes>
-//   </>
-//   )
-// }
-
-// export default App;
-
-
-
-
-
-
 
 
 import {Routes, Route ,Navigate} from "react-router";
@@ -76,18 +18,7 @@ import AdminUpload from "./components/AdminUpload"
 function App(){
   
   const dispatch = useDispatch();
-  // const {isAuthenticated,user,loading} = useSelector((state)=>state.auth);
-
-  // // check initial authentication
-  // useEffect(() => {
-  //   dispatch(checkAuth());
-  // }, [dispatch]);
   
-  // if (loading) {
-  //   return <div className="min-h-screen flex items-center justify-center">
-  //     <span className="loading loading-spinner loading-lg"></span>
-  //   </div>;
-  // }
 
 
   const { isAuthenticated, user, authChecked } = useSelector((state) => state.auth);
